5af8cfdab4ac550625f2f1bc19746f5816338d22
Aftab Alam <aftabalamdlm@gmail.com>
2025-04-15 00:31:58 +0530

    added env file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
@@ -7,6 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 
 require("./tasks/ping");
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
